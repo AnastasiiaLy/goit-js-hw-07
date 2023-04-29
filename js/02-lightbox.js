dbac6fdda0763a1b8de4025546f2355ab85c1f58
Anastasiia Lytus <litusnastja@gmail.com>
2023-04-29 19:45:53 +0300

    2nd hm is done

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,28 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const imagesList = document.querySelector(".gallery");
+
+const imagesMarkup = createImagesMarkup(galleryItems);
+
+imagesList.innerHTML = imagesMarkup;
+
+function createImagesMarkup(images) {
+  return images
+    .map(
+      (image) => `
+<li class="gallery__item">
+   <a class="gallery__link" href="${image.original}">
+      <img class="gallery__image" src="${image.preview}" alt="${image.description}" />
+   </a>
+</li>`
+    )
+    .join("");
+}
+
+const lightbox = new SimpleLightbox(".gallery__link", {
+  captionsData: "alt",
+  captionDelay: "250",
+});
